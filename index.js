346cb12f6bd9ad65138e898ac1ecb40b8e17754e
tmem11 <tmem.nsasra@gmail.com>
2021-08-15 00:20:08 +0300

    update database

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
 const  express =require('express');
 const mongoose =require('mongoose');
-//mongodb+srv://new_user:<password>@cluster0.i0wo2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-//JZMJ8BODYPIBJKUx
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc=require('swagger-jsdoc')
 
-url='mongodb://localhost/Ikea'||process.env.MONGODB_URI;
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 2000;
 const app=express();
+app.use(express.json())
  mongoose.connect('mongodb+srv://new_user:JZMJ8BODYPIBJKUx@cluster0.i0wo2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
      ,{useNewUrlParser:true,useUnifiedTopology:true});
 const con=mongoose.connection
 con.on('open',()=>{
     console.log('connected to the database!')
 })
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Hello World',
+            version: '1.0.0',
+        },
+        servers: [
+            {
+                url: "http://localhost:2000/",
+            },
+        ],
+    },
+    apis: ["./routes/*js"],
+
+};
+let swaggerDocument;
+swaggerDocument = swaggerJsDoc(swaggerOptions);
 const typesRouter=require('./routes/types');
-app.use(express.json())
 app.use('/types',typesRouter);
 const furnituresRouter=require('./routes/furnitures');
 app.use(express.json())
 app.use('/furnitures',furnituresRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument,{ explorer: true }));
+//aa
+/**
+ * @swagger
+ * /test:
+ * get:
+ * description: to test
+ * responses:
+ *  '200':
+ *         done!!
+ *
+ *
+ */
+
 
 
 
--- routes/types.js
+++ routes/types.js
@@ -1,6 +1,50 @@
 const  express =require('express');
 const router=express.Router()
 const  Type=require('../models/Type');
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Type:
+ *       type: object
+ *       required:
+ *         - name
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the types
+ *         name:
+ *           type: string
+ *           description: The type name
+ *       example:
+ *         id: 61180f4902fe53affbd43c2a
+ *         name: table
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Types
+ *   description: The Types managing API
+ */
+
+/**
+ * @swagger
+ * /types:
+ *   get:
+ *     summary: Returns the list of all the types
+ *     tags: [Types]
+ *     responses:
+ *       200:
+ *         description: The list of the types
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Type'
+ */
 router.get('/', async (req,res)=>{
     try {
         const types= await Type.find()
